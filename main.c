4ce40be894109aaed6f5ae408f3d59cbbef4f9fc
Clem <cle-ment-83200@hotmail.fr>
2018-11-25 16:40:19 +0100

    [INIT] Init of repo

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main (int argc, char argv[]){
+	int j = 0;
+	for (j=0; j<10; j=j+1){
+		printf("Count %d\n", j);
+	}
+return 0;
+}
